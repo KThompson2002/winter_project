96e5a24c39870f1cc88439f75a1ea6572c747581
KThompson2002 <kylezthompson@gmail.com>
2026-01-20 14:42:12 -0600

    Setup vision ROS2 package Setup Camera interface

---
+++ vlm_vision/vlm_vision/vision.py
@@ -0,0 +1,136 @@
+from enum import auto, Enum
+
+from cv_bridge import CvBridge
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+from rcl_interfaces.msg import ParameterDescriptor, ParameterType
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from sensor_msgs.msg import CameraInfo, Image
+
+
+class Vision(Node):
+    """Understand Scenes and convert into encoded information for VLMs to understand."""
+
+    def __init__(self):
+        """Initialize Vision Node."""
+        super().__init__('vision')
+
+        self.get_logger().info('vision init')
+        qos_profile = QoSProfile(depth=10)
+
+        self.declare_parameter(
+            'image_topic',
+            '/camera/image_raw',
+            ParameterDescriptor(type=ParameterType.PARAMETER_STRING)
+        )
+        self.declare_parameter(
+            'color_image_topic',
+            '/camera/camera/color/image_raw',
+            ParameterDescriptor(type=ParameterType.PARAMETER_STRING)
+        )
+        self.declare_parameter(
+            'depth_image_topic',
+            '/camera/camera/aligned_depth_to_color/image_raw',
+            ParameterDescriptor(type=ParameterType.PARAMETER_STRING)
+        )
+        self.declare_parameter(
+            'camera_info_topic',
+            '/camera/camera/color/camera_info',
+            ParameterDescriptor(type=ParameterType.PARAMETER_STRING)
+        )
+
+        self.image_topic = self.get_parameter('image_topic').value
+        self.color_image_topic = self.get_parameter('color_image_topic').value
+        self.depth_image_topic = self.get_parameter('depth_image_topic').value
+        self.camera_info_topic = self.get_parameter('camera_info_topic').value
+        
+        #Subscribers # noqa: E26
+        self.color_sub = Subscriber(
+            self,
+            Image,
+            self.color_image_topic
+        )
+        self.depth_sub = Subscriber(
+            self,
+            Image,
+            self.depth_image_topic
+        )
+        self.caminfo_sub = self.create_subscription(
+            CameraInfo,
+            self.camera_info_topic,
+            self.camera_info_callback,
+            qos_profile
+        )
+
+        self.ts = ApproximateTimeSynchronizer(
+            [self.color_sub, self.depth_sub],
+            queue_size=10,
+            slop=0.05  # 50 ms tolerance
+        )
+        self.ts.registerCallback(self.synced_callback)
+
+        #Timer callback # noqa: E26
+        self.timer = self.create_timer(0.01, self.timer_callback)
+
+        self.image_pub = self.create_publisher(
+            Image,
+            self.image_topic + '_axes',
+            10
+        )
+
+        #Attributes # noqa: E26
+        self.intrinsics = None
+        self.got_intrinsics = False
+        self.color_img = None
+        self.depth_img = None
+        self.bridge = CvBridge()
+
+    def timer_callback(self):
+        """Activates camera."""
+        if self.got_intrinsics:
+
+            if self.color_img is None or self.depth_img is None:
+                self.get_logger().warn('Waiting for images...')
+                return
+
+            color_img = self.bridge.imgmsg_to_cv2(
+                self.color_img,
+                desired_encoding='bgr8'
+            )
+
+            # depth_img = self.bridge.imgmsg_to_cv2(
+            #     self.depth_img,
+            # )
+
+            mask_msg = self.bridge.cv2_to_imgmsg(color_img)
+            self.image_pub.publish(mask_msg)
+
+    def synced_callback(self, color_msg, depth_msg):
+        """Sync callback for color and depth."""
+        self.color_img = color_msg
+        self.depth_img = depth_msg
+
+    def camera_info_callback(self, msg):
+        """Camera info callback."""
+        fx = msg.k[0]
+        fy = msg.k[4]
+        cx = msg.k[2]
+        cy = msg.k[5]
+
+        if not self.got_intrinsics:
+            self.intrinsics = (fx, fy, cx, cy)
+            self.got_intrinsics = True
+
+
+def main(args=None):
+    """Entry point for the vision node."""
+    rclpy.init(args=args)
+    node = Vision()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
